205741f18f3b6366f13147342361dbd047474743
Lars <t1xx3n@gmail.com>
2022-01-22 20:13:04 +0100

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Duration
+
+A description of this package.
---
+++ Tests/DurationTests/DurationTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Duration
+
+final class DurationTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Duration().text, "Hello, World!")
+    }
+}
