c6c1c48f54c7b76c13c65629cfbd4ae33e745f6b
Lars <t1xx3n@gmail.com>
2022-01-25 21:46:06 +0100

    fix: make duration error public

--- Sources/Duration/DurationError.swift
+++ Sources/Duration/DurationError.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 /// `DurationError` is the error type returned by `Duration`.
-enum DurationError: Error {
+public enum DurationError: Error {
     /// The provided string could not be parsed as `Duration`.  Valid formats are `PnYnMnDTnHnMnS` or `PnW`.
     case parsingError
 }
